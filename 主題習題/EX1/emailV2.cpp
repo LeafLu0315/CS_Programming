c18790775b4ce184785b205d725c403af0bdc3ea
leaflu0315 <johnson7511333@gmail.com>
2018-04-05 12:29:08 +0800

    Finished (?)

--- 主題習題/EX1/emailV2.cpp
+++ 主題習題/EX1/emailV2.cpp
@@ -9,6 +9,7 @@ struct EMAIL{
 /* Functions */
 bool dcmp(EMAIL,EMAIL);
 bool ucmp(EMAIL,EMAIL);
+bool udcmp(EMAIL,EMAIL);
 int duplicate(char [],char);
 int exists(char [],char);
 int judge(EMAIL);
@@ -17,64 +18,82 @@ int spacebetween(EMAIL);
 void Tlower(char []);
 /* Main */
 int main(){
-    int n,act,num=0,i,validnum=0;
+    int n,act,num,i,validnum,counter;
     EMAIL e[100],validemail[100];
-    freopen("email.in","r",stdin);
-    scanf("%d\n",&n);
-    while(n--){
-        gets(e[num].email);
-        cout<<e[num].email;
-        act=judge(e[num]);
-        switch(act){
-            case 0:
-                cout<<" =>is valid!"<<endl;
-                validemail[validnum++]=e[num];
-                break;
-            case 1:
-                cout<<" =>missing \'@\'!"<<endl;
-                break;
-            case 2:
-                cout<<" =>missing \'.\'!"<<endl;
-                break;
-            case 3:
-                cout<<" =>duplicate \'@\'!"<<endl;
-                break;
-            case 4:
-                cout<<" =>dots are nearby!"<<endl;
-                break;
-            case 5:
-                cout<<" =>user name contains invalid character!"<<endl;
-                break;
-            case 6:
-                cout<<" =>domain contains invalid character!"<<endl;
-                break;
-            case 7:
-                cout<<" =>user name is empty!"<<endl;
-                break;
-            case 8:
-                cout<<" =>nothing behind the dot!"<<endl;
-                break;
-            case 9:
-                cout<<" =>domain : there is space between the words!"<<endl;
-                break;
-            case 10:
-                cout<<" =>user name : there is space between the words!"<<endl;
-                break;
+    while(scanf("%d\n",&n),n){
+        num=validnum=0;
+        while(n--){
+            gets(e[num].email);
+            cout<<e[num].email;
+            act=judge(e[num]);
+            switch(act){
+                case 0:
+                    cout<<" =>is valid!"<<endl;
+                    validemail[validnum++]=e[num];
+                    break;
+                case 1:
+                    cout<<" =>missing \'@\'!"<<endl;
+                    break;
+                case 2:
+                    cout<<" =>missing \'.\'!"<<endl;
+                    break;
+                case 3:
+                    cout<<" =>duplicate \'@\'!"<<endl;
+                    break;
+                case 4:
+                    cout<<" =>dots are nearby!"<<endl;
+                    break;
+                case 5:
+                    cout<<" =>user name contains invalid character!"<<endl;
+                    break;
+                case 6:
+                    cout<<" =>domain contains invalid character!"<<endl;
+                    break;
+                case 7:
+                    cout<<" =>user name is empty!"<<endl;
+                    break;
+                case 8:
+                    cout<<" =>nothing behind the dot!"<<endl;
+                    break;
+                case 9:
+                    cout<<" =>domain : there is space between the words!"<<endl;
+                    break;
+                case 10:
+                    cout<<" =>user name : there is space between the words!"<<endl;
+                    break;
+            }
+            num++;
         }
-        num++;
+        puts("<!=============================================");
+        if(validnum>0){
+            char *tok;
+            for(i=0;i<validnum;i++){
+                counter=0;
+                tok=strtok(validemail[i].email,"@");
+                while(tok!=NULL){
+                    if(counter == 1) strcpy(validemail[i].domain,tok);
+                    else if(counter == 0) strcpy(validemail[i].username,tok);
+                    tok=strtok(NULL,"@");
+                    counter++;
+                }
+                strcat(validemail[i].email,"@");
+                strcat(validemail[i].email,validemail[i].domain);
+                Tlower(validemail[i].email);
+            }
+            cout<<"Sort by user name :"<<endl;
+            sort(validemail,validemail+validnum,ucmp);
+            for(i=0;i<validnum;i++) cout<<validemail[i].username<<"@"<<validemail[i].domain<<endl;
+            puts("=============================================");
+            cout<<"Sort by domain :"<<endl;
+            sort(validemail,validemail+validnum,dcmp);
+            for(i=0;i<validnum;i++) cout<<validemail[i].username<<"@"<<validemail[i].domain<<endl;
+            puts("=============================================");
+            cout<<"Sort by user name and domain :"<<endl;
+            sort(validemail,validemail+validnum,udcmp);
+            for(i=0;i<validnum;i++) cout<<validemail[i].username<<"@"<<validemail[i].domain<<endl;
+        }else cout<<"No email can be sorted"<<endl;
+        puts("=============================================!>\n");
     }
-    puts("<!=============================================");
-    if(validnum>0){
-        for(i=0;i<validnum;i++) Tlower(validemail[i].email);
-        cout<<"Sort by user name :"<<endl;
-        sort(validemail,validemail+validnum,ucmp);
-        for(i=0;i<validnum;i++) cout<<validemail[i].email<<endl;
-        puts("=============================================");
-        cout<<"Sort by domain :"<<endl;
-        sort(validemail,validemail+validnum,dcmp);
-        for(i=0;i<validnum;i++) cout<<validemail[i].email<<endl;
-    }else cout<<"No email can be sorted"<<endl;
-    puts("=============================================!>");
     return 0;
 }
 
@@ -86,11 +105,15 @@ bool ucmp(EMAIL a,EMAIL b){
     return strcmp(a.username,b.username)<0;
 }
 
+bool udcmp(EMAIL a,EMAIL b){
+    return (strcmp(a.username,b.username)<0 && strcmp(a.domain,b.domain)<0);
+}
+
 int judge(EMAIL e){
-    /* Not Exists @ or .*/
+    /* Not Exists @ or . */
     if(!exists(e.email,'@')) return 1;
     if(!exists(e.email,'.')) return 2;
-    /* Duplicate @ or Dots are nearby*/
+    /* Duplicate @ or Dots are nearby */
     if(duplicate(e.email,'@')) return 3;
     if(duplicate(e.email,'.')) return 4;
     /* Initialize */
@@ -98,11 +121,12 @@ int judge(EMAIL e){
     char *tok=strtok(e.email,"@");
     memset(e.domain,'\0',sizeof(e.domain));
     memset(e.username,'\0',sizeof(e.username));
-    /* Take out Domain and the UserName*/
+    /* Take out Domain and the UserName */
     while(tok!=NULL){
-        if(counter++) strcpy(e.domain,tok);
-        else strcpy(e.username,tok);
+        if(counter == 1) strcpy(e.domain,tok);
+        else if(counter == 0) strcpy(e.username,tok);
         tok=strtok(NULL,"@");
+        counter++;
     }
     /* Invalid character */
     char valid[]=".@_ ";
@@ -111,9 +135,9 @@ int judge(EMAIL e){
     for(s=e.domain;*s;s++) if(!isalnum(*s) && strchr(valid,*s) == NULL) return 6;
     /* Empty */
     if(!strcmp(e.domain,"")) return 7;
-    /* Nothing behind the dot*/
+    /* Nothing behind the dot */
     if(nothing(e)) return 8;
-    /* There are space between*/
+    /* There are space between */
     if(spacebetween(e) == 1) return 9;
     if(spacebetween(e) == 2) return 10;
     return 0;
