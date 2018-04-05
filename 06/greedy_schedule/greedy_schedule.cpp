ea1a6a51a84204c072e338e6bed86449a330859b
leaflu0315 <johnson7511333@gmail.com>
2018-04-05 21:30:18 +0800

    106-2 Programming

--- 06/greedy_schedule/greedy_schedule.cpp
+++ 06/greedy_schedule/greedy_schedule.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
+#define N 3000
 using namespace std;
-
+void sol();
+int t[N];
 int main(){
-
-
+    int n;
+    cin>>n;
+    while(n--) sol();
     return 0;
+}
+
+void sol(){
+    int time,machine,T,i,j,k;
+    cin>>time>>machine;
+    for(i=0;i<machine;i++) t[i]=0;
+    for(i=0;i<time;i++){
+        cin>>T;
+        for(j=0,k=1;k<machine;k++) if(t[k]<t[j]) j=k;
+        t[j]+=T;
+    }
+    for(i=0,j=0;i<machine;i++) if(t[i]>t[j]) j=i;
+    cout<<t[j]<<endl;
+    return ;
 }
--- 06/greedy_scheduleMany/greedy_scheduleMany.cpp
+++ 06/greedy_scheduleMany/greedy_scheduleMany.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
+#include<queue>
 using namespace std;
-
+void sol();
 int main(){
-
-
+    int n;
+    cin>>n;
+    while(n--) sol();
     return 0;
+}
+
+void sol(){
+    int time,machine,i,t,s;
+    priority_queue<int> pq;
+    cin>>time>>machine;
+    for(i=0;i<machine;i++) pq.push(0);
+    for(i=0;i<time;i++){
+        cin>>t;
+        s=pq.top();
+        pq.pop();
+        pq.push(s-t);
+    }
+    for(i=0;i<machine-1;i++) pq.pop();
+    cout<<-pq.top()<<endl;
+    pq.pop();
+    return ;
 }
