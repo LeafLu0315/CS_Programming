54d9ed327521c05617a6e6bbfc8cbc5b3bef9d6f
leaflu0315 <johnson7511333@gmail.com>
2018-05-06 17:29:46 +0800

    just a little bit

--- 主題習題/EX4/Integration.cpp
+++ 主題習題/EX4/Integration.cpp
@@ -2,6 +2,7 @@
 #include<cstring>
 #include<cmath>
 #include<cctype>
+/* functions */
 double integration(double(double),double,double,double);
 double poly(double);
 double mcalc(int,double,double,double);
@@ -9,32 +10,53 @@ void polyproc(char []);
 double power(double,int);
 const int cosize=10;
 int coefficient[cosize],cocounter=0;
+
 int main(int argv, char* argc[]){
+    /* give the maximum size */
     const int msize=13,polysize=50;
     char pol[polysize];
+    /* cMath's functions*/
     const char math[msize][6]={"sin","cos","tan","asin","acos","atan","atan2","cosh","sinh","tanh","acosh","asinh","atanh"};
+    /* Integrate from a to b and the width is t */
     double a,b,t,y=0;
     bool isMath;
     int i;
+    /* Judge if it's from command line or execution file */
     if(argv > 0){
         while(scanf("%s%lf%lf%lf",pol,&a,&b,&t)!=EOF){
+            /* The initial isMath will be false */
             isMath = false;
             for(i=0;i<msize;i++)
-                if(!strcmp(math[i],pol)){
+                if(!strcmp(math[i],pol)){   //if it compare to one of the cmath it will be true and record the position
                     isMath = true;
                     break;
                 }
-            if(isMath) y = mcalc(i,a,b,t);
+            if(isMath) y = mcalc(i,a,b,t);  //calculate the cmath's integration
             else{
-                polyproc(pol);
-                y = integration(poly,a,b,t);
+                polyproc(pol);  //processing the polynomials
+                y = integration(poly,a,b,t);    //calculate the integration
             }
-            printf("%f\n",y);
+            printf("%f\n",y);   //output the result
         }
+    }
+    else{   //CMD input
+        isMath = false;
+        sscanf(argc[i],"%s %lf %lf %lf",pol,&a,&b,&t);
+        for(i=0;i<msize;i++)
+            if(!strncmp(pol,math[i],sizeof(math[i])-1)){
+                isMath = true;
+                break;
+            }
+        if(isMath) y = mcalc(i,a,b,t);
+        else{
+            polyproc(pol);
+            y = integration(poly,a,b,t);
+        }
+        printf("%f\n",y);
     }
     return 0;
 }
-
+/* find which function to use */
 double mcalc(int pos,double a,double b,double t){
     double y = 0;
     switch(pos){
@@ -79,24 +101,24 @@ double mcalc(int pos,double a,double b,double t){
     }
     return y;
 }
-
+/* Calculate the sum */
 double integration(double (*f)(double), double a, double b,double t){
     double sum=0;
-    for(double i=a;i<=b;i+=t) sum+=f(i);
+    for(double i=a;i<=b;i+=t) sum +=f(i);
     return sum*t;
 }
-
+/* Processing the polynomial */
 void polyproc(char pol[]){
     const int tmpsize=5;
     int counter=0,num=0;
     char tmp[tmpsize]={'\0'};
     bool execution_once;
     cocounter = 0;
-    for(int i=0;i<cosize;i++) coefficient[i] = 0;
-    if(isdigit(pol[0])) tmp[counter++] = pol[0];
+    for(int i=0;i<cosize;i++) coefficient[i] = 0;   //initialize
+    if(isdigit(pol[0])) tmp[counter++] = pol[0];    //is digit or not
     for(int i=1;i<=strlen(pol);i++){
         execution_once = false;
-        if(isdigit(pol[i]) && pol[i-1] != '^') tmp[counter++] = pol[i];
+        if(isdigit(pol[i]) && pol[i-1] != '^') tmp[counter++] = pol[i]; //get the digit into the array
         else if(pol[i] == '+' || pol[i] == '-' || pol[i] == '\0'){
             for(int j = 0 ;j<counter;j++) {
                 if(execution_once) num*=10;
@@ -110,13 +132,13 @@ void polyproc(char pol[]){
     for(int i=0;i<cocounter;i++) if(coefficient[i] == 0) coefficient[i] = 1;
     return ;
 }
-
+/* calculate the f(x)*/
 double poly(double x){
     double sum = 0;
     for(int i=0;i<cocounter;i++) sum+=coefficient[i]*power(x,(cocounter-i-1));
     return sum;
 }
-
+/* power function*/
 double power(double x, int y){
     double sum = 1,xi=x;
     while(y){
