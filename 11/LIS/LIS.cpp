15edf19a911ef3727339b2f665b90be3889d6f02
leaflu0315 <johnson7511333@gmail.com>
2018-05-12 12:02:18 +0800

    106-2 Programming

--- 11/LIS/LIS.cpp
+++ 11/LIS/LIS.cpp
@@ -1,6 +1,24 @@
 #include<cstdio>
-
+#include<vector>
+using namespace std;
+void sol();
 int main(void){
-
+    int n;
+    scanf("%d",&n);
+    while(n--) sol();
     return 0;
+}
+
+void sol(){
+    int n;
+    vector<int> v;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        int tmp;
+        scanf("%d",&tmp);
+        vector<int>::iterator vi = lower_bound(v.begin(),v.end(),tmp);
+        if(vi == v.end()) v.push_back(tmp);
+        else *vi = tmp;
+    }
+    printf("%d\n",v.size());
 }
