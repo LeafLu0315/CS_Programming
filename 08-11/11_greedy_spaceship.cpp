f9f6379c3ca4676d02f91a02da0c6d3c9bbadc29
leaflu0315 <johnson7511333@gmail.com>
2018-05-24 23:58:15 +0800

    8-11 優良程式碼

--- 08-11/11_greedy_spaceship.cpp
+++ 08-11/11_greedy_spaceship.cpp
@@ -1,7 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(void){
-    freopen("greedy_spaceship.in","r",stdin);
-
+    int n;
+    while(scanf("%d",&n)){
+        if(n==0) break;
+        int tem1=1000,tem2=1000,ene1=0,ene2=0;
+        for(int i=0;i<n;i++){
+            int a,b;
+            scanf("%d%d",&a,&b);
+            int tmp=min(ene1+abs(tem1-a),ene2+abs(tem2-a));
+            ene2=min(ene1+abs(tem1-b),ene2+abs(tem2-b));
+            ene1=tmp;
+            tem1=a;
+            tem2=b;
+        }
+        printf("%d\n",min(ene1,ene2));
+    }
     return 0;
 }
