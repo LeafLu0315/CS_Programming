5b5b650e9948469a8c3781096c496cb9f2d04e51
leaflu0315 <johnson7511333@gmail.com>
2018-06-09 17:37:09 +0800

    I will use teacher's code

--- 主題習題/EX5/qsort.cpp
+++ 主題習題/EX5/qsort.cpp
@@ -2,52 +2,56 @@
 #include<cstdlib>
 #include<ctime>
 #include<memory.h>
-#define N 1000  //array size
-#define GENERATE_RANGE 100000000   //random number range
 /* functions */
-int random(int*,int,int);
-int cmp(void*,void*);
-int cmp2(int,int);
-void quick(int*,int,int,int (*cmp)(void*,void*));
+int random(int*,int,int,bool);
+bool cmp(int,int);
+bool cmp2(int,int);
+void quick(int*,int,int,bool (*cmp)(int,int));
 void myswap(int*,int*);
 void printArr(int*,int,bool);
-void generateArr(int*,int);
+bool rev = false;
 int main(void){
     srand(time(NULL));
-//    freopen("o.txt","w",stdout);
-    int *arr = new int[N];
-    generateArr(arr,N);
-    quick(arr,0,N-1,cmp);
-    bool rev = (cmp2(0,1)<0) ?  true:false;
-    printArr(arr,N,rev);
-    delete[] arr;
+//    FILE *file = fopen("random.bin","rb");
+    freopen("random.bin","rb",stdin);
+    freopen("out.txt","w",stdout);
+    int ncase,n;
+    scanf("%d",&ncase);
+    while(ncase--){
+        scanf("%d",&n);
+        int *arr = new int[n];
+        for(int i=0;i<n;i++) scanf("%d",&arr[i]);
+        quick(arr,0,n-1,cmp);
+        printArr(arr,n,rev);
+        delete[] arr;
+    }
     return 0;
 }
-/* generate array */
-void generateArr(int *arr,int n){
-    for(int i=0;i<n;i++) arr[i] = (rand()*rand())%GENERATE_RANGE;
-}
 /* compares */
-int cmp(void *a,void *b){
-    return *(int*)a-*(int*)b;
+bool cmp(int a,int b){
+    return a<b;
 }
-int cmp2(int a,int b){
-    return a-b;
+bool cmp2(int a,int b){
+    return a>b;
 }
 /* quick sort */
-void quick(int *arr,int start,int ending,int (*cmp)(void*,void*)){
+void quick(int *arr,int start,int ending,bool (*compare)(int,int)){
     if(start>=ending) return;
-    int pivot = random(arr,start,ending);
+    rev = compare(1,0);
+    int pivot = random(arr,start,ending,rev);
     quick(arr,start,pivot-1,cmp);
     quick(arr,pivot+1,ending,cmp);
 }
 /* Choose random pivot */
-int random(int *arr,int start,int ending){
+int random(int *arr,int start,int ending,bool rev){
     int pivotIndex = start+rand()%(ending-start+1); //generate index between start to ending
     int pivot = arr[pivotIndex];
     int left = start - 1;
     myswap(&arr[pivotIndex],&arr[ending]); //let pivot being new end
-    for(int i=start;i<ending;i++) if(arr[i] < pivot) myswap(&arr[++left],&arr[i]);
+    for(int i=start;i<ending;i++){
+        if(arr[i] < pivot && rev == false) myswap(&arr[++left],&arr[i]);
+        else if(arr[i] > pivot && rev == true) myswap(&arr[++left],&arr[i]);
+    }
     myswap(&arr[left+1],&arr[ending]);
     return left+1;  //random pivot
 }
