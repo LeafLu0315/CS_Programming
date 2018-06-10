c3c7e9a99cfc25b077261f3b866ee559e7c02cc1
leaflu0315 <johnson7511333@gmail.com>
2018-06-10 15:17:59 +0800

    add comment

--- 主題習題/EX6/queen.c
+++ 主題習題/EX6/queen.c
@@ -22,6 +22,7 @@ int main(){
         for(i=0;i<n;i++) for(j=0;j<n;j++) scanf("%d",&cell[i][j]);  // input the information
         if(n <= 0 || n == 2 || n == 3) printf("-1 -1"); // if n x n board is negative or {0,2,3} then there is no solution
         else if(n>N) printf("Not supported");
+        else if(n == 1) printf("%d %d",cell[0][0],cell[0][0]);
         else{ //else solve the problem 0 is max 1 is min
             solve(0);
             printf(" ");
