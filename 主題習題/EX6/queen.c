5181152b412dde8fafca97e705830642db703311
leaflu0315 <johnson7511333@gmail.com>
2018-06-10 14:49:30 +0800

    add comment

--- 主題習題/EX6/queen.c
+++ 主題習題/EX6/queen.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<memory.h>
 #define MAXIMUM -1000000
 #define N 25
 typedef struct{
@@ -11,16 +12,16 @@ int ABS(int);
 int MAX(int,int);
 int MIN(int,int);
 int NEGATIVE(int);
-int max,n;
+int ans,n;
 int cell[N][N],val[N];
 int main(){
     int ncase,i,j;
-//    freopen("eq_16.in","r",stdin);
     scanf("%d",&ncase);
     while(ncase--){
         scanf("%d",&n); //n*n board
         for(i=0;i<n;i++) for(j=0;j<n;j++) scanf("%d",&cell[i][j]);  // input the information
         if(n < 0 || n == 2 || n == 3) printf("-1 -1"); // if n x n board is negative or 2 or 3 then there is no solution
+        else if(n>N) printf("Not supported");
         else{ //else solve the problem 0 is max 1 is min
             solve(0);
             printf(" ");
@@ -36,8 +37,7 @@ void solve(int act){
     int i,j;
     if(act) for(i=0;i<n;i++) for(j=0;j<n;j++) cell[i][j]=NEGATIVE(cell[i][j]);   //when all are being negative then find the max is the min
     else for(i=0;i<n;i++) for(j=0;j<n;j++) cell[i][j]=ABS(cell[i][j]);   //when all are being negative then find the max is the min
-    /* Max */
-    max=MAXIMUM;
+    ans=MAXIMUM;
     for(i=0;i<n;i++){
         val[i]=MAXIMUM;
         for(j=0;j<n;j++) val[i] = MAX(val[i],cell[i][j]); //find the maximum in n0 n1 n2...nn
@@ -46,30 +46,28 @@ void solve(int act){
     for(i=0;i<n;i++) for(j=0;j<n;j++) x.bd[i][j]=1; // all are 1s
     x.row=x.cost=0; //initialize
     rec(x);
-    printf("%d",(act) ? -max:max);
+    printf("%d",(act) ? -ans:ans);
 }
 
-void rec(BOARD b){
-    int i,j,k;
-    BOARD t;
-    if(b.row == n){     // n queen is put then find the max
-        max = MAX(max,b.cost);
+void rec(BOARD x){
+    if(x.row == n){     // find the max
+        ans = MAX(ans,x.cost);
         return;
     }
-    if(b.cost+val[b.row]<=max) return;   //see whether down board have the best solution or not
+    if(x.cost+val[x.row]<=ans) return;   //see whether down board have the best solution or not
+    int i,j,k;
+    BOARD t;
     for(i=0;i<n;i++){
-        if(b.bd[b.row][i]){
-            t=b;
-            t.bd[t.row][i]=2;
+        if(x.bd[x.row][i]){
+            t=x;
+            t.bd[t.row][i]=2;       //0 => markup 1 => where they can put 2 => put queen
             t.cost+= cell[t.row++][i];
-            for(j=t.row;j<n;j++) t.bd[j][i]=0;
-            j=t.row;
-            k=i+1;
-            while(j<n && k<n) t.bd[j++][k++]=0;
-            j=t.row;
-            k=i-1;
-            while(j<n && k>=0) t.bd[j++][k--]=0;
-            rec(t);
+            for(j=t.row;j<n;j++) t.bd[j][i]=0;  //mark the column
+            j=t.row; k=i+1;
+            while(j<n && k<n) t.bd[j++][k++]=0; //mark the diagonal 00 11 22 33....
+            j=t.row; k=i-1;
+            while(j<n && k>=0) t.bd[j++][k--]=0; // mark another side of diagonal
+            rec(t); //keep the board and continue recursive
         }
     }
     return;
